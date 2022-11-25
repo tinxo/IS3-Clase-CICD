6d34747f07bd9da5e7f493e9e62df89bf8309fef
Martín Rey <tinxo@users.noreply.github.com>
2022-11-25 12:10:01 -0300

    Rompiendo algo

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def form():
 def result():
     # linea de ejemplo para hacer un commit
     # Se tienen que obtener los valores ingresados en el form
-    num_1 = request.form.get("num_1", type=int)
+    num_1 == request.form.get("num_1", type=int)
     num_2 = request.form.get("num_2", type=int)
     operador = request.form.get("operador", type=str)
     operaciones = {
